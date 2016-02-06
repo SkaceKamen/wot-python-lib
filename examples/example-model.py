b1192f6c76379d7f4f9b59c08c0501f2af037923
Jan Zípek <jan@zipek.cz>
2016-02-06 10:04:46 +0100

    Cleaned up example folder

--- examples/example-model.py
+++ examples/example-model.py
@@ -13,7 +13,8 @@ model_writer = wot.OBJModelWriter(
 	compress=False,
 	normals=True,
 	uv=True,
-	material=True
+	material=True,
+	scale=(-1,1,1)
 )
 
 # Change this to your world of tanks path
@@ -21,17 +22,15 @@ reader.setWotPath("d:/Hry/World_of_Tanks_closed_Beta/")
 reader.setCachePath("cache/")
 
 # Extract model files
-#reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Town_house_G01_01.primitives_processed", "temp.primitives")
-#reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Town_house_G01_01.visual_processed", "temp.visual")
-reader.extract("vehicles/chinese/Ch06_Renault_NC31/collision/Hull.visual_processed", "temp.visual")
-reader.extract("vehicles/chinese/Ch06_Renault_NC31/collision/Hull.primitives_processed", "temp.primitives")
+reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Town_house_G01_01.primitives_processed", "temp.primitives")
+reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Town_house_G01_01.visual_processed", "temp.visual")
 
 # Read model files
-with open("Hull.primitives_processed", "rb") as prim:
-	with open("Hull.visual_processed", "rb") as vis:
+with open("temp.primitives", "rb") as prim:
+	with open("temp.visual", "rb") as vis:
 		model = model_reader.read(prim, vis)
 		model_writer.write(model, "temp.obj")
 
 # Remove temp files			
-#os.unlink("temp.primitives")
-#os.unlink("temp.visual")
+os.unlink("temp.primitives")
+os.unlink("temp.visual")
