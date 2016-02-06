4664099926d67062a5e6d26eec4f85e18126e908
Jan Zípek <jan@zipek.cz>
2016-02-06 12:18:34 +0100

    Added compability with python3

--- wot/chunks/bwst.py
+++ wot/chunks/bwst.py
@@ -1,4 +1,4 @@
-from utility import *
+from wot.chunks.utility import *
 
 def get(f, debug=False):
 	strings = {}
@@ -13,7 +13,7 @@ def get(f, debug=False):
 	for item in table["entries"]:
 		key = unp("<I", item[0:4])
 		f.seek(strings_start + unp("<I", item[4:8]))
-		strings[key] = f.read(unp("<I", item[8:12]))
+		strings[key] = f.read(unp("<I", item[8:12])).decode("UTF-8")
 		
 		if debug:
 			print (hex2(key, 8), strings[key])
