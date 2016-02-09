3a5c88c955c5cffba1372249e3a81b5af05ee0c5
Jan Zípek <jan@zipek.cz>
2016-02-09 11:13:05 +0100

    Added collada writer. Fixed wrong order of indices.

--- examples/example-model.py
+++ examples/example-model.py
@@ -5,21 +5,34 @@ sys.path.append('..')
 import wot
 import os
 
+# Change this to your world of tanks path
+reader = wot.PackageReader()
+reader.setWotPath("d:/Hry/World_of_Tanks_closed_Beta/")
+reader.setCachePath("cache/")
+
+# This callback extracts texture to cache
+def extractTexture(path, type):
+	global reader
+	
+	name = os.path.split(path)[1]
+	result = "cache/" + name
+	
+	reader.extract(path, result)
+	
+	return result
+
 # Use this class to read from world of tanks packages,
 # without need of finding and extracting required resources
-reader = wot.PackageReader()
 model_reader = wot.ModelReader(True)
-model_writer = wot.OBJModelWriter(
+model_writer = wot.ColladaModelWriter(
 	compress=False,
 	normals=True,
 	uv=True,
 	material=True,
-	scale=(-1,1,1)
+	scale=(-1,1,1),
+	textureCallback=extractTexture
 )
-
-# Change this to your world of tanks path
-reader.setWotPath("d:/Hry/World_of_Tanks_closed_Beta/")
-reader.setCachePath("cache/")
+	
 
 # Extract model files
 reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Town_house_G01_01.primitives_processed", "temp.primitives")
@@ -29,7 +42,7 @@ reader.extract("content/Buildings/bldEU_001_Town_house/normal/lod0/bldEU_001_Tow
 with open("temp.primitives", "rb") as prim:
 	with open("temp.visual", "rb") as vis:
 		model = model_reader.read(prim, vis)
-		model_writer.write(model, "temp.obj")
+		model_writer.write(model, "temp.dae")
 
 # Remove temp files			
 os.unlink("temp.primitives")
